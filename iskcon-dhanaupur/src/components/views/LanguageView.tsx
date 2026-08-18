3f179908cf197c7cab62b604c9e5c70a3adc10de
iskcondhanaupur <iskcon.dhanaupur@gmail.com>
2026-08-18 10:49:06 +0530

    logo

--- iskcon-dhanaupur/src/components/views/LanguageView.tsx
+++ iskcon-dhanaupur/src/components/views/LanguageView.tsx
@@ -132,7 +132,7 @@ export default function LanguageView({ onSelect }: Props) {
           style={{ marginBottom: 24 }}
         >
           <Image
-            src="/welcomenote.png"
+            src="/n.png"
             alt="ISKCON Dhanaupur"
             width={380}
             height={280}
